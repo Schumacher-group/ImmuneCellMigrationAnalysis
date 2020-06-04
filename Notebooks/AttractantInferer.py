a09ac8c1ad6e9463ec7016ffd9c7e0ec55ee1c0c
Daniel Tudor <danieltudor273@gmail.com>
2020-06-04 16:26:25 +0100

    Cleaned up to include correct time bins and remove step error

--- Notebooks/AttractantInferer.py
+++ Notebooks/AttractantInferer.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 ## This takes in the Random Walker inferred parameters and outputs a dictionary with the observed bias parameters
 
 def Bias_persistance(x,y):
-    Dataset = np.load('../data/np_array/WB total WT-{}{}.npy'.format(x,y))
+    Dataset = np.load('../data/np_array/WB WT-{}{}.npy'.format(x,y))
     W = Dataset[:,0]
     B = Dataset[:,2]
     OB = (W * B)
@@ -25,7 +25,7 @@ def Bias_persistance(x,y):
 
 
 distance = [25,50,75,100,125,150,175]
-time = [5,10,30,50]
+time = [5,10,30,45]
 
 ob_readings = {}
 for i in range(len(time)):
