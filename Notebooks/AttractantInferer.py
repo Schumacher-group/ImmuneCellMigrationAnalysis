bf4bc6a246b8ca8558ab713d949f4a456fd0db94
Daniel Tudor <danieltudor@daniels-mbp.home>
2020-06-09 15:53:47 +0100

    Weavers paper criteria

--- Notebooks/AttractantInferer.py
+++ Notebooks/AttractantInferer.py
@@ -17,32 +17,41 @@ def Bias_persistance(x,y):
     Dataset = np.load('../data/np_array/WB WT-{}{}.npy'.format(x,y))
     W = Dataset[:,0]
     B = Dataset[:,2]
-    OB = (W * B)
-    mean = np.mean(OB)
-    std = np.std(OB)
+    #WMut= Dataset[1][:,0]
+    #BMut = Dataset[1][:,2]
 
-    return mean,std
+    OBWT = (W * B)
+    #OBMUT = (WMut * BMut)
+
+    return OBWT#, OBMUT
+
 
 
 distance = [25,50,75,100,125,150,175]
-time = [5,10,30,45]
-
+time = [5,10,30,45]#,40,50]
+means=[]
 ob_readings = {}
 for i in range(len(time)):
     for j in range(len(distance)):
         data = Bias_persistance(j,i)
-        mean = data[0]
-        std = data[1]
+        mean = np.mean(data)
+        std = np.std(data)
+        means.append(mean)
         ob_readings[(distance[j], time[i])] = (mean,std)
+
+
+
+
+
 
 # Attractant inference
 wound = PointWound(position=np.array([0, 0]))
 inferer = AttractantInferer(ob_readings, wound=wound, t_units='minutes')
-out1 = inferer.multi_infer(n_walkers=10,
+out1 = inferer.multi_infer(n_walkers=16,
                             n_steps=500000,
                             burn_in=500000,
                             seed=0,
                             suppress_warnings=True,
                             use_tqdm=True)
 #Saves the current attractant inference numpy array for processing
-np.save('../data/AttractantInferenceWTMixedPriors_fixedstep',out1)s
+np.save('../data/AttractantInferenceWTMixedPriors',out1)
