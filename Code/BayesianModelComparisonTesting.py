65a5c1fefee7e779c004af32f79544b219d0773a
Daniel Tudor <danieltudor273@gmail.com>
2021-08-03 13:09:31 +0100

    Testing file for Bayesian Model comparison, currently only testing that the observed bias is calling the correct diffusion equation model

---
+++ Code/BayesianModelComparisonTesting.py
@@ -0,0 +1,28 @@
+import unittest
+import pandas as pd
+import numpy as np
+from pathlib import Path
+from Data_cleaning import wound_locations, filelocation, reformat_file, csv_to_dataframe
+from inference.attractant_inference import AttractantInferer, observed_bias
+from in_silico.sources import CellsOnWoundMargin, PointWound, CellsInsideWound
+
+class TestDataCleaning(unittest.TestCase):
+    def test_observed_bias_true(self):
+        tau = 28
+        R0 = 0.2
+        kappa = 0.5
+        m = 3
+        b0 = 0.001
+        wound = PointWound(position=np.array([0, 0]))
+        delta_params = np.array([10, 20, R0, kappa, m, b0])
+        production_params = np.array([1000, 500, tau, R0, kappa, m, b0])
+
+        r = 25
+        tt = 10
+        ActualResult = observed_bias(delta_params, r, tt, wound, model="delta")
+        ExpectedResult = observed_bias(delta_params, r, tt, wound, model="delta")
+        self.assertEqual(ActualResult, ExpectedResult, 'Different models chosen')
+
+
+if __name__ == '__main__':
+    unittest.main()
