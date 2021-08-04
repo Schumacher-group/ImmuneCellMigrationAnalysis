c02a7317dd62f0c00b2b523a066fc9171a482865
Daniel Tudor <danieltudor273@gmail.com>
2021-08-04 10:09:31 +0100

    Added new function for plotting inferred observed bias from posterior parameters called observed_bias_posterior_plots

--- Code/PlotsForPublish.py
+++ Code/PlotsForPublish.py
@@ -1,5 +1,4 @@
 import matplotlib.pyplot as plt
-import seaborn as sns
 import arviz as az
 import numpy as np
 from pathlib import Path
@@ -40,7 +39,7 @@ def observed_bias_plots(x_start, x_stop, t_start, t_stop, param, wound_loc, mode
     axes[-1].set_xlabel('Distance, microns')
     plt.tight_layout()
     if save_fig == True:
-        plt.savefig('../data/Synthetic_Data/observed_bias.pdf', format='pdf')
+        plt.savefig(f'../data/Synthetic_Data/observed_bias_{model}.pdf', format='pdf')
     plt.show()
 
     def scatter(scatters):
@@ -56,18 +55,59 @@ def observed_bias_plots(x_start, x_stop, t_start, t_stop, param, wound_loc, mode
     return scatter(scatters)
 
 
-def plot_posterior(sampler, variables, num_vars, name, save_fig=False, **kwargs):
+def observed_bias_posterior_plots(x_start, x_stop, t_start, t_stop, true_param, post_param, wound_loc, true_model,post_model, save_fig=False,
+                             **kwargs):
+    # noinspection DuplicatedCode
+    from inference.attractant_inference import observed_bias
+
+    # instantiate a point wound
+
+    # where to measure observed bias
+    r_points = np.arange(x_start, x_stop, x_start)
+
+    r = np.linspace(x_start, x_stop + x_start, 100)
+    t = np.arange(t_start, t_stop, 20)
+    samples = post_param
+    fig, axes = plt.subplots(ncols=1, nrows=len(t), sharex=True)
+
+    for ax, p in zip(axes, t):
+        ax.set_ylabel('$t={}$'.format(p), rotation=0, size='large', labelpad=35)
+
+    # plot the points
+    scatters = []
+    for i, tt in enumerate(t):
+        col = plt.rcParams['axes.prop_cycle'].by_key()['color'][i]
+        scatters.append(
+            axes[i].plot(r_points, observed_bias(true_param, r_points, tt, wound_loc, model=true_model),
+                         color=col,
+                         marker='o', linewidth=0, markersize=6, label = 'True observed bias')[0])
+        for theta in samples[np.random.randint(len(samples), size=50)]:
+            axes[i].plot(r, observed_bias(theta, r, tt, wound_loc, model=post_model), color="r", alpha=0.1)
+
+        axes[i].set_ylim(0, 0.3)
+
+    axes[0].set_title(f'Observed bias from {post_model} attractant model')
+    axes[-1].set_xlabel('Distance, microns')
+    axes[len(t)-1].legend()
+    plt.tight_layout()
+    if save_fig == True:
+        plt.savefig(f'../data/Synthetic_Data/observed_bias_{post_model}_posterior.pdf', format='pdf')
+    plt.show()
+
+
+def plot_posterior_distributions(sampler, variables, num_vars, name, save_fig=False, **kwargs):
     var_names = variables
     emcee_data = az.from_emcee(sampler[0], var_names=var_names).sel(draw=slice(100, None))
-    az.plot_posterior(emcee_data, var_names=var_names[:])
+    plt.suptitle(f"Posterior distributions from {name}")
 
+    az.plot_posterior(emcee_data, var_names=var_names[:])
     if save_fig == True:
         plt.savefig(f'../data/Synthetic_Data/posterior_plots_{name}.pdf', format='pdf')
     plt.show()
 
 
-def plot_posterior_chains(sampler, params, name,n_discards, save_fig=False, **kwargs):
-    samples = sampler[0].get_chain(flat = True, discard = n_discards)
+def plot_posterior_chains(sampler, params, name, n_discards, save_fig=False, **kwargs):
+    samples = sampler[0].get_chain(flat=True, discard=n_discards)
     fig, axes = plt.subplots(nrows=len(params), ncols=1, figsize=(12, 5), sharex='col')
     cols = plt.rcParams['axes.prop_cycle'].by_key()['color']
 
