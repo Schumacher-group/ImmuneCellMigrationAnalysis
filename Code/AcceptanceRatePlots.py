61430d450da62d11768f2db34e19421326cc9e28
Daniel Tudor <danieltudor273@gmail.com>
2021-09-10 11:40:43 +0100

    Test for recommended number of movies for Yi

--- Code/AcceptanceRatePlots.py
+++ Code/AcceptanceRatePlots.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-acceptance_rate_production = np.load('../data/Synthetic_Data/acceptance_rate_production_delta_60walkers.npy', allow_pickle=True)
-acceptance_rate_delta = np.load('../data/Synthetic_Data/acceptance_rate_delta_delta_60walkers.npy', allow_pickle=True)
+acceptance_rate_production = np.load('../data/Synthetic_Data/acceptance_rate_production_deltamodel_60walkers_large.npy', allow_pickle=True)
+acceptance_rate_delta = np.load('../data/Synthetic_Data/acceptance_rate_delta_deltamodel_60walkers_large.npy', allow_pickle=True)
 n_walkers = np.load('../data/Synthetic_Data/number_of_walkers.npy', allow_pickle=True)
 BayesFactor = np.load('../data/Synthetic_Data/BayesFactor.npy', allow_pickle=True)
 mean_production = np.empty(len(acceptance_rate_production))
@@ -18,11 +18,11 @@ plt.plot(acceptance_rate_production,'o-', label='Production model')
 plt.plot(acceptance_rate_delta,'o-', label='Delta model')
 plt.plot(mean_production, 'k--', label='Mean production accept rate')
 plt.plot(mean_delta, 'k-.', label='Mean delta accept rate')
-
+plt.title("Observed bias data from delta model")
 plt.xlabel('Iterations')
 plt.ylabel('Acceptance rate')
 plt.legend()
-plt.savefig(f'../data/Synthetic_Data/60_walkers_average_acceptance.pdf', format='pdf')
+plt.savefig(f'../data/Synthetic_Data/60_walkers_average_acceptance_deltamodel.pdf', format='pdf')
 
 plt.show()
 
--- Code/Mix_persistences_accuracy.py
+++ Code/Mix_persistences_accuracy.py
@@ -5,8 +5,8 @@ sys.path.append(os.path.abspath('..'))
 
 import datetime
 
-from inference.walker_inference_mixed_persistence import BiasedPersistentInferer as BiasedPersistentInfererMix
-
+from inference.walker_inference_mixed_persistence import BiasedPersistentInferer
+import matplotlib.pyplot  as plt
 from in_silico.walkers import BP_Leukocyte
 from in_silico.sources import PointSource
 
@@ -15,15 +15,15 @@ import numpy as np
 
 source = PointSource(position=np.array([0, 0]))
 
-t = np.arange(30, 330, 30)
+t = np.arange(15, 165, 15)
 
 np.random.seed(100)
-w1, p1, b1 = 0.5, 0.4, 0.6
+w1, p1, b1 = 0.5, 0.7, 0.7
 params1 = np.array([w1, p1, b1])
 walker1 = BP_Leukocyte(params1, source)
 
-w2, p2 = 0.5, 0.8
-params2 = np.array([w2, p2, b1])
+p2 = 0.3
+params2 = np.array([w1, p2, b1])
 walker2 = BP_Leukocyte(params2, source)
 
 p1_paths_list = []
@@ -39,17 +39,35 @@ for step in t:
     p1_paths_list.append(paths_p1)
     p2_paths_list.append(paths_p2)
 
-total_paths_list = [np.concatenate((p1_paths_list[i], p2_paths_list[i]), axis = 2 ) for i in range(len(p1_paths_list))]
+total_paths = [np.concatenate((p1_paths_list[i], p2_paths_list[i]), axis = 2 ) for i in range(len(p1_paths_list))]
 
 
 niter = 18000 # number of MCMC iterations
 nwalkers = 90 # number of
 iters = 0
 for i in range(len(t)):
-    inferer_t = BiasedPersistentInfererMix(total_paths_list[i], source)
-    total_inf = inferer_t.ensembleinfer(nwalkers, niter)
-    np.save(f'../data/Mixed_persistence_accuracy/total_list_vals{i}_for_Yi', total_inf)
-    print(f"{i}/{len(t)}")
+    inferer_total = BiasedPersistentInferer(total_paths[i], source)
+    niter = 2000  # number of MCMC iterations
+    nwalkers = 140  # number of
+    EM_total = inferer_total.ensembleinfer(nwalkers, niter)
+
+    sampler = EM_total[0]
+    p_total = sampler.get_chain(discard=1000, thin=1, flat=True)
+    P1 = p_total[:, 2]
+    P2 = p_total[:, 3]
+    np.save(f"/Users/danieltudor/Documents/GitHub/ImmuneCellMigrationAnalysis/data/Mixed_persistence_accuracy/EM_total{i}",
+            EM_total)
+    plt.hist(P1, label='$P_1$ = {:.2f} $\pm$ {:.2f}'.format(np.mean(P1), np.std(P1)), bins=100, alpha=0.6, density=True)
+    plt.hist(P2, label='$P_2$ = {:.2f} $\pm$ {:.2f}'.format(np.mean(P2), np.std(P2)), bins=100, alpha=0.6, density=True)
+
+    plt.axvline(0.3, color='black', ls='--', label="True value: 0.3")
+    plt.axvline(0.7, color='black', ls='--', label="True value: 0.7")
+    plt.xlabel("Persistence parameter", fontsize=16)
+    plt.ylabel("Probability Density", fontsize=16)
+    plt.legend()
+    plt.savefig(
+        f"/Users/danieltudor/Documents/GitHub/ImmuneCellMigrationAnalysis/data/Mixed_persistence_accuracy/Persistence_total_mix_w2_p1_p2_{i}.pdf")
+    plt.show()
 
 
 datetime_object = datetime.datetime.now()
@@ -57,7 +75,7 @@ datetime_object = datetime.datetime.now()
 file = open(f'../data/Mixed_persistence_accuracy/Param_values_{datetime_object}', 'w')
 file.write('Parameter values')
 file.write('----------------')
-file.write(f'biases = {b1,b2}, persistence 1 = {p1}, persistence 2 = {p2} , weights = {w1,w2}')
+file.write(f'biases = {b1}, persistence 1 = {p1}, persistence 2 = {p2} , weights = {w1}')
 file.write('Length of walk = 60')
 file.write(f'Number of walkers = {t}')
 file.close()
