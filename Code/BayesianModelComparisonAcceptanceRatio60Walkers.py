727eccde5bc69257a50f1913c505761cd5019bae
dtudor <dtudor@ed.ac.uk>
2021-09-10 09:02:57 +0100

    delta test case

--- Code/BayesianModelComparisonAcceptanceRatio60Walkers.py
+++ Code/BayesianModelComparisonAcceptanceRatio60Walkers.py
@@ -12,8 +12,8 @@ params_delta = [80, 200, 0.2, 0.5, 3, 0.001]
 params_production = [600, 400, 35, 0.2, 0.5, 3, 0.001]
 wound = PointWound(position=np.array([0, 0]))
 
-ob_readings = observed_bias_plots(25, 300, 10, 140, params_production, wound, 'production', save_fig=True)
-Num_walkers  = 80
+ob_readings = observed_bias_plots(25, 300, 10, 140, params_delta, wound, 'delta', save_fig=True)
+Num_walkers  = 60
 Num_acceptance_rate_production = []
 Num_acceptance_rate_delta = []
 Bayes_Factors = []
@@ -32,6 +32,6 @@ for num in range(50):
     Bayes_Factors.append(BayesFactor(post_production[0], post_delta[0], n_discards=7000))
 
 
-save_accept_production = np.save('../data/Synthetic_Data/acceptance_rate_production_delta_80walkers_large',Num_acceptance_rate_production)
-save_accept_delta = np.save('../data/Synthetic_Data/acceptance_rate_delta_delta_80walkers_large', Num_acceptance_rate_delta)
+save_accept_production = np.save('../data/Synthetic_Data/acceptance_rate_production_deltamodel_60walkers_large',Num_acceptance_rate_production)
+save_accept_delta = np.save('../data/Synthetic_Data/acceptance_rate_delta_deltamodel_60walkers_large', Num_acceptance_rate_delta)
 save_bayes_factor = np.save('../data/Synthetic_Data/BayesFactor_delta_80walkers_large',Bayes_Factors)
